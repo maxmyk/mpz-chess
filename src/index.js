dea0903e12674085761f80642b664ae314e4f95c
Bohdan213 <bogdanpeleh213@gmail.com>
2023-05-15 01:09:08 +0300

    routing added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Demo from "./Demo";
+import Home from './Home';
+import About from './About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<h1>404 - Not Found</h1>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
