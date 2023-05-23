baa95f94baa0c16445f181420f518ed1c16e1fc6
maxmyk <maksym.mykhasiuta@ucu.edu.ua>
2023-05-24 01:12:01 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import './App.css';
-// import React, { useEffect } from "react";
 import React from "react";
 import WithMoveValidation from "./integrations/validator";
-// const socket = require('./integrations/socket').socket
+import { BrowserRouter as Router, Route, Routes, useParams, Navigate } from "react-router-dom";
 
-function App() {
-  // useEffect(() => {
-  //   socket.on("receive_move", (data) => {
-  //     alert(data)
-  //   });
-  // }, [socket]);
+const GameMultiplayer = () => {
+  let { room_id } = useParams();
+
+  // Generate a random room number if room_id is not present
+  if (!room_id) {
+    room_id = Math.floor(Math.random() * 1000000).toString();
+    return <Navigate to={`/game_multiplayer/${room_id}`} />;
+  }
+
+  // Use the room_id value in your component logic
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <h1>Multiplayer Game</h1>
+      <p>Room ID: {room_id}</p>
+      {/* Render the rest of your multiplayer game */}
+    </div>
+  );
+};
+
+
+function App() {
+
+  return (
+    <header className="App-header">
+      <Routes>
+        <Route exact path="/game_multiplayer" component={GameMultiplayer} />
+        <Route path="/game_multiplayer/:room_id" component={GameMultiplayer} />
         <h1>Really cool multiplayer chess</h1>
         <div>Send this link to your friend:</div>
         <a href={window.location.href}>{window.location.href}#your_unique_id</a>
         <div style={boardsContainer}>
           <WithMoveValidation />
         </div>
-      </header>
-    </div>
+      </Routes>
+    </header>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Home from './Home';
+import GameMultiplayer from './GameMultiplayer';
 import Statistic from './Statistic';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route exact path="/game_multiplayer" element={<App />} />
-        <Route exact path="/game_bot" element={<App />} />
         <Route path="/" element={<Home />} />
+        <Route path="/game_bot" element={<App />} />
+        <Route path="/game_multiplayer/*" element={<GameMultiplayer />} />
         <Route path="/statistic" element={<Statistic />} />
-        <Route element={<h1>404 - Not Found</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   </React.StrictMode>,
