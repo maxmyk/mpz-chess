74b5222d063bfec1c16770cc7edda6a002588931
Bohdan213 <bogdanpeleh213@gmail.com>
2023-05-23 15:25:56 +0300

    I don't remember what i did here

--- src/Statistic.js
+++ src/Statistic.js
@@ -1,10 +1,43 @@
 import React from "react";
+import io from "socket.io-client";
+import { useState } from "react";
+
+const socket = io.connect("http://10.10.241.217:3001");
 
 const Statistic = () => {
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  const [room, setRoom] = useState("");
+
+  const [message, setMessage] = useState("");
+  // const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
   return (
-    <div id="stats">
-      <h1>User statistic</h1>
-      <p>Here will be your statistic</p>
+    <div>
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <div>
+        <input
+          placeholder="Message..."
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+        />
+        <button onClick={sendMessage}> Send Message</button>
+      </div>
     </div>
   );
 };
